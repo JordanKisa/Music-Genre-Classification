6936d223b47785478bda277a31c747843300db1f
Jordan  Kisa <jordan.kisa18@imperial.ac.uk>
2023-08-17 05:54:47 +0100

    Script to split audio files into equal lengths

---
+++ src/helpers/split_audio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 input_directory output_directory"
+    exit 1
+fi
+
+input_directory="$1"
+output_directory="$2"
+
+mkdir -p "$output_directory"
+
+for file in "$input_directory"/*.mp3; do
+    filename=$(basename "$file")
+    
+    ffmpeg -i "$file" -f segment -segment_time 10 -c copy "$output_directory/${filename%.mp3}_%03d.mp3"
+done

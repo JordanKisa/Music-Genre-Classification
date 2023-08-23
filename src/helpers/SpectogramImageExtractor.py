5e4870f592ac9c27d4709ff0b6c45c340f0a3c86
Jordan  Kisa <jordan.kisa18@imperial.ac.uk>
2023-08-23 03:37:06 +0100

    Updated extractor helpers to run concurrently

--- src/helpers/SpectogramImageExtractor.py
+++ src/helpers/SpectogramImageExtractor.py
@@ -6,6 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm  # Import tqdm for tracking progress
 
+import warnings
+warnings.filterwarnings("ignore")
+
+
 def generate_and_save_spectrograms(music_directory, spectrogram_directory):
     # Create the spectrogram directory if it doesn't exist
     if not os.path.exists(spectrogram_directory):
@@ -13,25 +17,35 @@ def generate_and_save_spectrograms(music_directory, spectrogram_directory):
 
     # Loop through music files
     for filename in tqdm(os.listdir(music_directory)):
-        if filename.endswith('.mp3'):
+        if filename.endswith(".mp3"):
             file_path = os.path.join(music_directory, filename)
-            
+
             # Load the audio file
             y, sr = librosa.load(file_path)
-            
+
             # Compute the spectrogram
-            spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
-            spectogram_db = librosa.amplitude_to_db(spectrogram)
+            chroma = librosa.feature.chroma_stft(y=y, sr=sr)
             
+            tempo, beats = librosa.beat.beat_track(y=y, sr=sr, hop_length=512)
+            beats = librosa.util.fix_frames(beats, x_min=0)
+            chroma_sync = librosa.util.sync(chroma, beats)
+
+            chroma_lag = librosa.feature.stack_memory(
+                chroma_sync, n_steps=2, mode="edge"
+            )
+            split_index = chroma_lag.shape[0] // 2
+            temporal_chroma = chroma_lag[:split_index] + chroma_lag[split_index:]
+
             # Save the spectrogram as an image
             spectrogram_image_path = os.path.join(spectrogram_directory, f'{os.path.splitext(filename)[0]}.png')
-            plt.imsave(spectrogram_image_path, spectogram_db, cmap='cool')
+            plt.imsave(spectrogram_image_path, temporal_chroma)
 
-    print('Spectrogram generation and saving completed.')
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python generate_spectrograms.py music_directory spectrogram_directory")
+        print(
+            "Usage: python generate_spectrograms.py music_directory spectrogram_directory"
+        )
         sys.exit(1)
 
     music_files_directory = sys.argv[1]
--- src/helpers/SpectrogramDataExtractor.py
+++ src/helpers/SpectrogramDataExtractor.py
@@ -3,29 +3,36 @@ import sys
 import librosa
 import numpy as np
 import pandas as pd
-from tqdm import tqdm  # Import tqdm for tracking progress
+from tqdm import tqdm
+import concurrent.futures  # Import concurrent.futures for concurrency
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def generate_mel_spectrogram(music_directory,filename):
+    file_path = os.path.join(music_directory, filename)
+    
+    # Load the audio file
+    y, sr = librosa.load(file_path)
+    
+    # Compute the mel spectrogram
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+    mel_spectrogram_db = librosa.amplitude_to_db(mel_spectrogram).tobytes()
+    
+    return os.path.splitext(filename)[0], mel_spectrogram_db
 
 def generate_and_save_mel_spectrograms(music_directory, csv_filename):
     mel_spectrograms = []
-    labels = []
+    titles = []
+    total = len(os.listdir(music_directory))
 
-    # Loop through music files
-    for filename in tqdm(os.listdir(music_directory)):
-        if filename.endswith('.mp3'):
-            file_path = os.path.join(music_directory, filename)
-            
-            # Load the audio file
-            y, sr = librosa.load(file_path)
-            
-            # Compute the mel spectrogram
-            mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
-            mel_spectrogram_db = librosa.amplitude_to_db(mel_spectrogram)
-            
-            mel_spectrograms.append(mel_spectrogram_db.flatten())  # Flatten the mel spectrogram
-            labels.append(os.path.splitext(filename)[0])  # Use the filename as the label
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for filename, mel_spectrogram in tqdm(executor.map(generate_mel_spectrogram, [music_directory] * total, os.listdir(music_directory)), total=total):
+            mel_spectrograms.append(mel_spectrogram)  # Flatten the mel spectrogram
+            titles.append(filename)  # Use the filename as the label
 
     # Create a DataFrame to store the flattened mel spectrograms and labels
-    mel_data = pd.DataFrame({'mel_spectrogram': mel_spectrograms, 'label': labels})
+    mel_data = pd.DataFrame({'title': titles,  'spectrogram': mel_spectrograms, 'label': [os.path.basename(os.path.dirname(music_directory))] * total})
 
     # Save DataFrame to a CSV file
     mel_data.to_csv(csv_filename, index=False)
